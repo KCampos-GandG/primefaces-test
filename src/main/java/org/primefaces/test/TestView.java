78c2f556c560234d7e5bacca4eec0d60d58f2c42
KCampos-GandG <ikendall.ca@gmail.com>
2022-08-25 12:42:44 -0600

    menu

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,28 +1,46 @@
 package org.primefaces.test;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.application.ViewExpiredException;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import lombok.Data;
-import org.primefaces.event.CellEditEvent;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
 
 @Data
 @Named
 @RequestScoped
 public class TestView {
 
+    private MenuModel model;
     private String string;
 
     @PostConstruct
     public void init() {
         string = "PrimeFaces";
+        model = new DefaultMenuModel();
+        DefaultSubMenu firstSubmenu = DefaultSubMenu.builder()
+                .label("Options")
+                .build();
+        DefaultMenuItem item = DefaultMenuItem.builder()
+                .value("Delete")
+                .icon("pi pi-times")
+                .command("#{testView.delete}")
+                .build();
+        firstSubmenu.getElements().add(item);
+        model.getElements().add(firstSubmenu);
+        
+    }
+    
+    public void delete() {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Delete", "Data deleted");
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
     public void throwViewExpiredException() {
